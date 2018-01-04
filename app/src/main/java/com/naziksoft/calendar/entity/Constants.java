9535b18c0b1e76d54d362d8110280c3b40c493fb
morozione@gmail.com <morozione@gmail.com>
2018-01-04 11:21:12 +0200

    maked calendar week

---
+++ app/src/main/java/com/naziksoft/calendar/entity/Constants.java
@@ -0,0 +1,12 @@
+package com.naziksoft.calendar.entity;
+
+/**
+ * Created by nazar on 27.12.17.
+ */
+
+public class Constants {
+    public static final int ID_BUTTON_EDIT = 1;
+    public static final int ID_BUTTON_DELETE = 2;
+    public static final int ID_BUTTON_SPLIT = 3;
+    public static final String LOG = "CalendarLog";
+}
---
+++ app/src/main/java/com/naziksoft/calendar/entity/StatisticControl.java
@@ -0,0 +1,11 @@
+package com.naziksoft.calendar.entity;
+
+/**
+ * Created by nazar on 28.12.17.
+ */
+
+public interface StatisticControl {
+    void showStatisticLayout();
+    void hideStatisticLayout();
+    void setEventTime(String time);
+}
